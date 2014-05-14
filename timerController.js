f480810b2fbf5a05450c9c467c4fd6f4fef5d09f
wlu <wei.luo@zuhlke.com>
2014-05-14 15:12:46 +0100

    added timer for pomodoro, short break and long break

--- timerController.js
+++ timerController.js
@@ -1,6 +1,9 @@
 angular.module('pomodoroFactory', [])
     .controller('timerController', ['$scope', '$timeout', function ($scope, $timeout) {
-        var fullPomodoroInSec = 25 * 60;
+        var pomodoroInSec = 25 * 60;
+        var shortBreakInSec = 5 * 60;
+        var longBreakInSec = 15 * 60;
+
         $scope.secToDisplayValue = function (sec) {
             var minutes = Math.floor(sec / 60);
             var seconds = sec % 60;
@@ -10,17 +13,48 @@ angular.module('pomodoroFactory', [])
             return minutes + ':' + seconds;
         };
 
-        $scope.time = fullPomodoroInSec;
-        $scope.displayTime = $scope.secToDisplayValue($scope.time);
+        $scope.resetDisplay = function (timeInSec) {
+            $scope.time = timeInSec;
+        };
+
+        $scope.isInTimer = false;
+        $scope.resetDisplay(pomodoroInSec);
 
         $scope.onTimeOut = function () {
             $scope.time--;
-            $scope.displayTime = $scope.secToDisplayValue($scope.time);
             if ($scope.time > 0) {
-                $timeout($scope.onTimeOut, 1000);
+                $scope.myTimeout = $timeout($scope.onTimeOut, 1000);
             }
         };
 
+        $scope.startTimer = function (timeInSec) {
+            $scope.cancelPomodoro(timeInSec);
+            $scope.isInTimer = true;
+            $scope.myTimeout = $timeout($scope.onTimeOut, 1000);
+        };
 
-        $timeout($scope.onTimeOut, 1000);
+        $scope.startPomodoro = function () {
+            $scope.startTimer(pomodoroInSec);
+        };
+
+        $scope.startShortBreak = function () {
+            $scope.startTimer(shortBreakInSec);
+        };
+
+        $scope.startLongBreak = function () {
+            $scope.startTimer(longBreakInSec);
+        };
+
+        $scope.cancelPomodoro = function (timeToResetInSec) {
+            if (!timeToResetInSec) {
+                timeToResetInSec = pomodoroInSec;
+            }
+            if ($scope.isInTimer) {
+                $scope.isInTimer = false;
+                $timeout.cancel($scope.myTimeout);
+            }
+            $scope.resetDisplay(timeToResetInSec);
+        };
+
+
     }]);
